49f7787cc71641dfa73eb38aae72928e9e95c246
Adam Brock <a.paulbrock@gmail.com>
2022-07-05 11:34:23 -0400

    Initial commit of file structure

---
+++ utils/seed.js
@@ -0,0 +1,10 @@
+const connection = require('../config/connection');
+const { } = require('../models');
+const { } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  process.exit(0);
+});
