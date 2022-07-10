7db31151540e223f8f7c02e3d4bcce900cc54e89
Adam Brock <a.paulbrock@gmail.com>
2022-07-09 23:56:47 -0400

    Changed _id in the Reaction schema to be explicitly named (reactionId)

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,19 +1,27 @@
 const { Schema, Types } = require("mongoose");
+const ObjectId = require("mongodb").ObjectId;
 
-const reactionSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: ObjectId,
+      default: new ObjectId(),
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    reactionBody: { type: String, required: true },
+    username: { type: String, required: true },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
   },
-  reactionBody: { type: String, required: true },
-  username: { type: String, required: true },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    required: true,
-  },
-});
+  { _id: false }
+);
 
 const Reaction = reactionSchema;
 
